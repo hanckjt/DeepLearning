4685954684048495a868cb25320630de6f2c0d83
Hank <hanckjt@hotmail.com>
2019-06-12 18:50:23 +0800

    test

--- Tensorflow/test/test1.py
+++ Tensorflow/test/test1.py
@@ -8,17 +8,31 @@ from MnistTest import MnistTest
 
 
 def trainModel(x_train, y_train):
-    layers = [keras.layers.Conv2D(16, (3, 3), input_shape=(28, 28), activation=keras.activations.relu),
-              keras.layers.Conv2D(16, (3, 3), input_shape=(28, 28), activation=keras.activations.relu),
-              keras.layers.MaxPool2D(),
-              keras.layers.Flatten(),
-              keras.layers.Dense(512, activation=keras.activations.relu),
-              keras.layers.Dropout(0.2),
-              keras.layers.Dense(10, activation=keras.activations.softmax)]
+    layers = [
+        keras.layers.Conv2D(64, (3, 3), input_shape=(28, 28, 1), activation=keras.activations.relu),
+        keras.layers.MaxPool2D(),
+        keras.layers.Dropout(0.5),
+
+        keras.layers.Conv2D(128, (3, 3), input_shape=(28, 28, 1), activation=keras.activations.relu),
+        keras.layers.MaxPool2D(),
+        keras.layers.Dropout(0.5),
+
+        keras.layers.Conv2D(256, (3, 3), input_shape=(28, 28, 1), activation=keras.activations.relu),
+        keras.layers.MaxPool2D(),
+        keras.layers.Dropout(0.5),
+
+        keras.layers.Flatten(),
+        keras.layers.Dense(128, activation=keras.activations.relu),
+        keras.layers.Dense(64, activation=keras.activations.relu),
+        keras.layers.Dense(32, activation=keras.activations.relu),
+        keras.layers.Dropout(0.2),
+
+        keras.layers.Dense(10, activation=keras.activations.softmax)
+    ]
 
     model = keras.models.Sequential(layers)
     model.compile(optimizer=keras.optimizers.Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x_train.reshape((1, x_train.shape[0], 28, 28)), y_train.reshape((1, y_train[0], 1)), epochs=5)
+    model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=20, batch_size=1000)
 
     return model
 
@@ -28,7 +42,8 @@ class TestNet:
         self.model = model
 
     def predict(self, data):
-        return model.predict(data.reshape((1, 28, 28)))
+        return model.predict(data.reshape((1, 28, 28, 1)))
+
 
 if __name__ == '__main__':
     model_file = 'mnist.mod'
@@ -36,7 +51,7 @@ if __name__ == '__main__':
         model = keras.models.load_model(model_file)
     else:
         (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-        x_train, x_test = x_train / 255.0, x_test / 255.0
+        x_train, x_test = x_train.reshape(x_train.shape[0], 28, 28, 1) / 255.0, x_test.reshape(x_test.shape[0], 28, 28, 1) / 255.0
         model = trainModel(x_train, y_train)
         model.evaluate(x_test, y_test)
         model.save(model_file)
